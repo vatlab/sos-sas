0d20208f91e3828906822afed0525f9166df2c43
Bo Peng <ben.bog@gmail.com>
2020-04-16 20:47:19 -0500

    Allow importing dataset under customized libname

--- src/sos_sas/kernel.py
+++ src/sos_sas/kernel.py
@@ -4,11 +4,8 @@
 # Distributed under the terms of the 3-clause BSD License.
 
 import os
-import shutil
 import pandas as pd
 import argparse
-import tempfile
-import saspy
 import subprocess
 import uuid
 from sos.utils import env
@@ -46,23 +43,10 @@ class sos_SAS(SASsession):
         # we intentionally do not call SASsessionSTDIO's constructor, which will start SAS
         # We however need to fake stdin, stderr etc in order to work with the session
 
-        #self.pid    = None
-        #self.stdin  = None
-        #self.stderr = None
-        #self.stdout = None
-
-        #self.sascfg   = SASconfigSTDIO(**kwargs)
-        #self._log_cnt = 0
-        #self._log     = ""
-        #self._sb      = kwargs.get('sb', None)
-
-        #self._startsas()
         self.stdin = BytesIO()
         self.stderr = BytesIO()
 
         self.pid = None
-        # mimick a sascfg object with argparse Namespace
-        #self.sascfg = argparse.Namespace()
 
         self._sb = argparse.Namespace()
         self._sb.sascfg = sas_config
@@ -74,49 +58,28 @@ class sos_SAS(SASsession):
         self._log = ""
 
         self.sascfg = SASconfigSTDIO(self)
-        # self.sascfg.encoding = 'utf-8'
-        # self.sascfg.output = 'text'
-        # self.sascfg.verbose = False
-        # self.sascfg.tunnel = None
 
-        # self._sb = argparse.Namespace()
-        # self._sb._dsopts = lambda x: ''
-        # self._sb.file_info = lambda remotefile, quiet: True
-        # self._sb.HTML_Style = 'Default'
-
-    def submit(self, code, results='html', prompt=None):
-        logn = self._logcnt()
-        logcodei = f"%put %upcase(e3969440a681a24088859985{logn});"
-        logcodeo = f"E3969440A681A24088859985{logn}"
-
-        mj = ";*\';*\";*/;" + logcodei
+    def submit(self, code='', results='html', prompt=None):
         # get code to be executed from stdin, and add the submit code.
-        code = self.stdin.getvalue().decode('utf-8') + code
+        sas_code = self.stdin.getvalue().decode('utf-8')
         # clear stdin
         self.stdin.seek(0)
         self.stdin.truncate()
 
-        sas_code = mj + '\n' + code + '\n' + mj
+        sas_code += code
 
         env.log_to_file('Executing in SAS Kernel\n{}'.format(sas_code))
 
-        response = self.sos_kernel.get_response(
-            sas_code, ['stream', 'execute_result'],
-            name=('stdout', 'stderr', 'data'))
-        # response = self.sos_kernel.run_cell(
-        #     sas_code,
-        #     True,
-        #     False,
-        #     on_error=f"Failed to execute SAS code {sas_code}")
+        # the sas-kernel only has execute_result, nothing else
+        response = self.sos_kernel.get_response(sas_code, ('execute_result', ),
+                                                name=('data', ))
         res = [
-            x[1]['data']['text/html']
-            for x in response
+            x[1]['data']['text/html'] for x in response
             if x[0] == 'execute_result'
         ]
-        if res:
-            res = res[0]
-        else:
-            res = ''
+        res = res[0] if res else ''
+        env.log_to_file(f'RESPONSE\n {response}')
+
         return {'LOG': res, 'LST': res}
 
     def get_vars(self, names):
@@ -132,9 +95,10 @@ class sos_SAS(SASsession):
                 continue
             # sas cannot handle columns with non-string header
             data = env.sos_dict[name].rename(
-                columns={x: str(x) for x in env.sos_dict[name].columns})
+                columns={x: str(x)
+                         for x in env.sos_dict[name].columns})
             # convert dataframe to SAS, this function will call self.submit,
-            # which we have changed to submit to the underlying SAS kernel
+            # which we have changed to submit to the underlying SAS kernel.
             self.dataframe2sasdata(data, name, "")
 
     def scp_tmp_file(self, filename, temp_name):
@@ -154,7 +118,7 @@ class sos_SAS(SASsession):
             params += ["-i", self.sascfg.identity]
 
         params += [self.sascfg.host + ':' + filename, dest_file]
-        env.log_to_file('KERNEL', f'COMMAND "{pgm}" "{params}"')
+        env.log_to_file('KERNEL', f'Copy .sas2bdat file with commands "{" ".join(params)}"')
 
         subprocess.call(params)
         if not os.path.isfile(dest_file):
@@ -164,52 +128,52 @@ class sos_SAS(SASsession):
         return dest_file
 
     def put_vars(self, items, to_kernel=None):
-        # create a temporary directory
-        temp_name = uuid.uuid4().hex[:8]
         res = {}
         for idx, item in enumerate(items):
+            temp_name = uuid.uuid4().hex
             try:
                 # the directory will be created if it does not exist
+                if '.' in item:
+                    if item.count('.') > 1:
+                        raise ValueError(f'Name of SAS datasets can only be name or libname.name: {item} specified')
+                    libname, data = item.split('.', 1)
+                else:
+                    libname = 'work'
+                    data = item
+                # get the directory of the library
                 self.stdin.write(f'''\
-%put {temp_name}=%sysfunc(getoption(work));
+%put {temp_name}=%sysfunc(pathname({libname}));
 '''.encode('utf-8'))
                 # run the code to save file
-                resp = self.submit('')
+                resp = self.submit()
                 # let us get the path to the library
                 env.log_to_file('KERNEL', resp['LST'])
-                path_name = resp['LST'].split(temp_name + '=')[-1].split('<')[0]
+                path_name = resp['LST'].split(temp_name +
+                                              '=')[-1].split('<')[0]
 
-                # self.sos_kernel.run_cell(
-                #     code,
-                #     True,
-                #     False,
-                #     on_error="Failed to get data set {} from SAS".format(
-                #         item))
-                # #
-                # check if file exists
-                saved_file = os.path.join(path_name, f'{item.lower()}.sas7bdat')
-                if os.path.isfile(saved_file):
-                    # good, local file or shared file system
-                    # now try to read it with Python
+                data_file = os.path.join(path_name,
+                                          f'{data.lower()}.sas7bdat')
+
+                if os.path.isfile(data_file):
+                    # share the same file system
                     df = pd.read_sas(saved_file, encoding='utf-8')
-                    res[item] = df
+                    res[item.replace('.', '_')] = df
                 elif sas_config.mode == 'SSH':
                     # SSH, let us copy the file.
-                    saved_file = self.scp_tmp_file(saved_file, temp_name)
-                    df = pd.read_sas(saved_file, encoding='utf-8')
-                    res[item] = df
-                    os.remove(saved_file)
+                    local_data_file = self.scp_tmp_file(data_file, temp_name)
+                    df = pd.read_sas(local_data_file, encoding='utf-8')
+                    res[item.replace('.', '_')] = df
+                    os.remove(local_data_file)
                 else:
                     self.sos_kernel.warn(
                         '''Failed to access saved file. This version of sos-sas only
-                        support retrieving datasets from SAS server that shares the
+                        support retrieving datasets from SAS servers that share the
                         same file system as the Jupyter server, or with SSH connection.'''
                     )
                     continue
             except Exception as e:
                 self.sos_kernel.warn(
                     'Failed to get dataset {} from SAS: {}'.format(item, e))
-
         return res
 
     def parse_response(self, html):
@@ -232,5 +196,5 @@ PROC PRODUCT_STATUS;
 run;
 '''
         response = self.sos_kernel.get_response(
-            sas_code, ('execute_result',))[0][1]['data']['text/html']
+            sas_code, ('execute_result', ))[0][1]['data']['text/html']
         return self.parse_response(response)['LOG']
